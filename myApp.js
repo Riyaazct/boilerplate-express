b528b434ac649c22fa8830c602520824c7bfd4b3
Riyaaz Singh <riyaazct1@gmail.com>
2022-11-19 22:32:28 +0200

    Second commit

--- myApp.js
+++ myApp.js
@@ -1,8 +1,15 @@
 let express = require("express");
 let app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send(console.log("Hello World"));
+  res.sendFile(__dirname + "/views/index.html");
 });
+
+// app.get("/", (req, res) => {
+//   console.log("Hello World")
+//   res.send("Hello Express");
+// })
 
 module.exports = app;
